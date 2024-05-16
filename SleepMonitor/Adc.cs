df86374f797140dcfbd4d3c0f3cdee7f37c517d1
LaerkeSchelde <202301744@post.au.dk>
2024-05-16 11:57:52 +0200

    Ændringer i Main - test med file

--- SleepMonitor/Adc.cs
+++ SleepMonitor/Adc.cs
@@ -16,9 +16,7 @@ namespace SleepMonitor
 
         private Mcp3008 mcp;
         private static Timer timer;
-
-
-
+        
         // Field: Number of single ended input channel on the ADC
         protected new byte ChannelCount;
 
@@ -30,7 +28,7 @@ namespace SleepMonitor
         public Adc()
         {
             // Creating a new HW Spi object with two parameters, the busId and chipSelectLine
-            var hardwareSpiSettings = new SpiConnectionSettings(0, 0);
+            // ** var hardwareSpiSettings = new SpiConnectionSettings(0, 0);
             // i2cdetect -y 1
 
             // ** kode modtaget af Lars Mortensen: 
@@ -40,8 +38,8 @@ namespace SleepMonitor
             //};
 
             // The object that actively communicates with the device, take the previous object as a parameter
-            SpiDevice spi = SpiDevice.Create(hardwareSpiSettings);
-            mcp = new Mcp3008(spi); // instantiere
+            // ** SpiDevice spi = SpiDevice.Create(hardwareSpiSettings);
+            // ** mcp = new Mcp3008(spi); // instantiere
            
         }
 
--- SleepMonitor/Program.cs
+++ SleepMonitor/Program.cs
@@ -5,9 +5,27 @@ namespace SleepMonitor
 {
     public class Program
     {
+        Adc adc = new Adc();
         static void Main(string[] args)
         {
-          
+            string files = Directory.GetCurrentDirectory();
+            string[] files1 = Directory.GetFiles(files);
+            string SmData = "C://Users//Programmering//Programmering s2//Søvnmonimoni//SleepMonitor//monimoni.txt";
+            string[] lines = File.ReadAllLines(SmData);
+            foreach (string file in lines)
+            {
+                Console.WriteLine(file);
+            }
+            using (StreamReader sr = new StreamReader(SmData))
+            {
+                while (sr.EndOfStream != true)
+                {
+                    string? line = sr.ReadLine();
+                    Console.WriteLine($"{line}");
+                }
+            }
+
+
         }
     }
 }
