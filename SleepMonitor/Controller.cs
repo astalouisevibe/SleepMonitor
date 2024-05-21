4f3108b5612527805a31748adebf7a567d1f5d85
LaerkeSchelde <202301744@post.au.dk>
2024-05-21 14:53:50 +0200

    Controller og ADC er kopieret over i solution 2

--- SleepMonitor/Controller.cs
+++ SleepMonitor/Controller.cs
@@ -20,84 +20,20 @@ namespace SleepMonitor
 {
     public class Controller
     {
-        public double Threshold = 0.5; // tilpasset
-        public Stopwatch stopwatch = new Stopwatch();
-        public Converter converter = new Converter();
+        //public double Threshold = 0.5; // tilpasset
 
-        public List<Measurement> Measurements { get; private set; } = new List<Measurement>();
-        public List<AverageMeasurement> AverageMeasurements { get; private set; } = new List<AverageMeasurement>();
-
-        private Adc adc;
-
-        public Controller(Adc adc)
-        {
-            this.adc = adc;
-        }
-
-        public void StartReading()
-        {
-            stopwatch.Start();
-            Task.Run(() =>
-            {
-                while (true)
-                {
-                    double value = adc.ReadDigitalValue();
-
-                    Measurements.Add(new Measurement { Timestamp = DateTime.Now, Value = value });
-
-                    //if 5 min passed run update 
-                    if (stopwatch.Elapsed.TotalMinutes >= 5)
-                    {
-                        var outofbed = Analysedata(); // split list into 5 get average and then return true if it worked
-                        if (outofbed)
-                            if (stopwatch.Elapsed.TotalMinutes >= 5)
-                            {
-                                CalculateAndStoreAverage();
-                                stopwatch.Restart();
-                            }
-
-                        Thread.Sleep(250); // Assuming measurements are taken every 250ms
-                    }
-                }
-            });
-        }
-
-
-        private void CalculateAndStoreAverage()
-        {
-            var lastFiveMinutesMeasurements = Measurements.Where(m => m.Timestamp >= DateTime.Now.AddMinutes(-5)).ToList();
-            double averageValue = lastFiveMinutesMeasurements.Average(m => m.Value);
-            DateTime firstMeasurementTime = lastFiveMinutesMeasurements.First().Timestamp;
-
-            AverageMeasurements.Add(new AverageMeasurement { Timestamp = firstMeasurementTime, AverageValue = averageValue });
-            SaveAverageMeasurementsToJson();
-        }
-
-        private void SaveAverageMeasurementsToJson()
-        {
-            string jsonFilePath = "..\\..\\..\\Sleepdata.json";
-            var dataToSave = AverageMeasurements.Select(a => new
-            {
-                TimeStamp = a.Timestamp.ToString("yyyy-MM-dd HH:mm"),
-                AverageValue = a.AverageValue
-            }).ToList();
-
-            string json = JsonConvert.SerializeObject(dataToSave, Formatting.Indented);
-            File.WriteAllText(jsonFilePath, json);
-        }
+        //public List<Measurement> Measurements { get; private set; } = new List<Measurement>();
+        // Listen 'Measurement' ligger i solution 2
+        //public List<FiveMinMeas> AverageMeasurements { get; private set; } = new List<FiveMinMeas>();
     }
 
-    public class Measurement
-    {
-        public DateTime Timestamp { get; set; }
-        public double Value { get; set; }
-    }
+    //public class Measurement
+    //{
+    //    public DateTime Timestamp { get; set; }
+    //    public double Value { get; set; }
+    //}
 
-    public class AverageMeasurement
-    {
-        public DateTime Timestamp { get; set; }
-        public double AverageValue { get; set; }
-    }
+    
 }
-}
+
 
